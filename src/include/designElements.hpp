9320c7c7eb2a49b44452fa17c2722398e3568543
Code Gabe <gabrielcristiandan@hotmail.com>
2026-02-09 20:35:17 +0000

    Change const to constexpr for screen and font sizes

--- src/include/designElements.hpp
+++ src/include/designElements.hpp
@@ -1,8 +1,8 @@
 // Screen dimensions
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
 
 // Font sizes
-const int FONT_SIZE_SMALL = 12;
-const int FONT_SIZE_MEDIUM = 16;
-const int FONT_SIZE_LARGE = 20;
+constexpr int FONT_SIZE_SMALL = 12;
+constexpr int FONT_SIZE_MEDIUM = 16;
+constexpr int FONT_SIZE_LARGE = 20;

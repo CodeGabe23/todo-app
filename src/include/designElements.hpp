501dea949a90be48bd50ceb8f11d8cca5d5e1826
roaming <wilhelmus4321@gmail.com>
2026-02-09 19:28:30 +0000

    chore: foundational project structure

---
+++ src/include/designElements.hpp
@@ -0,0 +1,14 @@
+// Screen dimensions
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+// Font sizes
+const int FONT_SIZE_SMALL = 12;
+const int FONT_SIZE_MEDIUM = 16;
+const int FONT_SIZE_LARGE = 20;
+
+// Colors for the todo app
+const Color COLOR_BACKGROUND = WHITE;
+const Color COLOR_TEXT = BLACK;
+const Color COLOR_ACCENT = SKYBLUE;
+const Color COLOR_HIGHLIGHT = LIGHTGRAY;
